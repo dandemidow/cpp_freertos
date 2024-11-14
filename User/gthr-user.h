34870c833c0ebd0fbb0af87f9dfcf76e08b8fd92
Danila Demidov <dandemidow@gmail.com>
2024-11-14 21:38:48 +0300

    implement std::mutex regular

--- User/gthr-user.h
+++ User/gthr-user.h
@@ -3,43 +3,67 @@
 
 #include <ctime>
 #include <sys/_pthreadtypes.h>
+#include <errno.h>
+
+extern "C" {
+#include <FreeRTOS.h>
+#include <semphr.h>
+}
 
 using __gthread_time_t = struct timespec;
 using __gthread_cond_t = pthread_cond_t;
-using __gthread_mutex_t = int;
+//using __gthread_mutex_t = int;
 using __gthread_t = pthread_t;
 using __gthread_recursive_mutex_t = int;
 using __gthread_once_t = int;
 
+#define __GTHREAD_ONCE_INIT 0
 #define __GTHREAD_COND_INIT_FUNCTION
 
-#define __GTHREAD_ONCE_INIT 0
-#define __GTHREAD_MUTEX_INIT 0
+// #define __GTHREAD_MUTEX_INIT 0
 #define __GTHREAD_MUTEX_INIT_FUNCTION(mx) do {} while (0)
 #define __GTHREAD_RECURSIVE_MUTEX_INIT 0
 
+typedef struct gthreadMutexType {
+	StaticSemaphore_t mutexBuffer;
+	SemaphoreHandle_t mutex;
+} __gthread_mutex_t;
+
+inline void __gthread_mutex_init_func (__gthread_mutex_t *__mutex) {
+    __mutex->mutex = xSemaphoreCreateMutexStatic(&__mutex->mutexBuffer);
+}
+
 inline int __gthread_mutex_lock (__gthread_mutex_t *__mutex) {
-  return 0;
+  BaseType_t result {xSemaphoreTake(__mutex->mutex, portMAX_DELAY)};
+  return result == pdTRUE ? 0 : EINVAL;
 }
 
 inline int __gthread_mutex_trylock (__gthread_mutex_t *__mutex) {
-  return 0;
+  BaseType_t result {xSemaphoreTake(__mutex->mutex, 0)};
+  return static_cast<int>(result == pdTRUE);
 }
 
 inline int __gthread_mutex_unlock (__gthread_mutex_t *__mutex) {
+  BaseType_t result {xSemaphoreGive(__mutex->mutex)};
+  return result == pdTRUE ? 0 : EINVAL;
+}
+
+inline int __gthread_mutex_destroy (__gthread_mutex_t *__mutex) {
+  vSemaphoreDelete(__mutex->mutex);
   return 0;
 }
 
+
 inline int __gthread_recursive_mutex_lock (__gthread_recursive_mutex_t *__mutex) {
-  return __gthread_mutex_lock (__mutex);
+  return 0;//__gthread_mutex_lock (__mutex);
 }
 
 inline int __gthread_recursive_mutex_trylock (__gthread_recursive_mutex_t *__mutex) {
-  return __gthread_mutex_trylock (__mutex);
+  return 0;//__gthread_mutex_trylock (__mutex);
 }
 
 inline int __gthread_recursive_mutex_unlock (__gthread_recursive_mutex_t *__mutex) {
-  return __gthread_mutex_unlock (__mutex);
+  return 0;//__gthread_mutex_unlock (__mutex);
 }
 
 
